728bac96338584ea60306c1b7e2f49489f745011
mqas1 <114903874+mqas1@users.noreply.github.com>
2023-01-02 23:12:51 +1100

    adds content to the README file and comments to the code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Question array containing objects with question, choices, and the answer.
 var questions = [
     {
         title: "Commonly used data types DO NOT include:",
@@ -26,6 +27,7 @@ var questions = [
     }
 ];
 
+// Declaring variables to dynamically interact with the DOM.
 var startButton = document.querySelector("#start");
 var startEl = document.querySelector("#start-page");
 var timerEl = document.querySelector("#timer-count");
@@ -34,9 +36,12 @@ var answerEl = document.querySelector("#answer-list");
 var messageEl = document.querySelector("#msg");
 
 var timerInterval = 0;
+// Variable used to determine the object index in the question array.
 var quizIndex = 0;
+// Determines the time by adding 15 seconds to the clock for every question in the array.
 var secondsLeft = questions.length * 15;
 
+// Click event to start the timer and display the first question.
 startButton.addEventListener("click", function(){
     startEl.textContent = "";
 
@@ -44,6 +49,7 @@ startButton.addEventListener("click", function(){
         secondsLeft--;
         timerEl.textContent = secondsLeft;
 
+// If the clock reaches zero it will not continue to decrement instead finishing the game.
         if (secondsLeft <= 0) {
             clearInterval(timerInterval);
             quizComplete();
@@ -53,6 +59,7 @@ startButton.addEventListener("click", function(){
     displayQuiz(quizIndex);
 });
    
+// Creates list items for the question choices by manipulating the DOM.
     answerEl.setAttribute("style", "visibility: hidden");
 
     var li1 = document.createElement("li");
@@ -65,6 +72,9 @@ startButton.addEventListener("click", function(){
     answerEl.appendChild(li3);
     answerEl.appendChild(li4);
 
+// Function for iterating over the question array and manipulating the DOM
+// to display the question and its answers.
+// The quizIndex variable is used as a parameter in the function to determine the first question.
 function displayQuiz(quizIndex){
     for (var i = 0; i < questions.length; i++) {
         var currentQuestion = questions[quizIndex].title;
@@ -83,9 +93,13 @@ function displayQuiz(quizIndex){
 
         questionEl.setAttribute("style", "font-size: 32px; font-weight: 700; padding: 0rem 8rem 1rem 12rem");
     }
+// Click event on the quiz questions.
     answerEl.addEventListener("click", compareChoice);
 }
 
+// Function for comparing the user choice with the correct answer.
+// Each time the user makes a choice the question index is increased by 1
+// continuing until there are no more questions left in the array to answer.
 function compareChoice(event) {
    
     var element = event.target;
@@ -109,6 +123,8 @@ function compareChoice(event) {
 
     quizIndex++;
     
+// If there is another question in the array it will be displayed after
+// each question is answered by the user.
     if (quizIndex >= questions.length) {
         quizComplete();
     } else {
@@ -116,6 +132,8 @@ function compareChoice(event) {
     }
 }
 
+// Function for manipulating the DOM to create the text box to submit the user
+// score to local storage.
 function quizComplete(){
     questionEl.textContent = "";
     answerEl.textContent = "";
@@ -129,6 +147,7 @@ function quizComplete(){
     startEl.appendChild(completeH2);
 
     var completeP = document.createElement("p");
+// Turns the quiz timer into the user score.
     var timeScore = secondsLeft;
     completeP.textContent = "Your final score is " + timeScore + ".";
     startEl.appendChild(completeP);
@@ -147,27 +166,35 @@ function quizComplete(){
     completeSubmit.setAttribute("style", "margin-left: 0.5rem");
     startEl.appendChild(completeSubmit);
 
+// Click event on the submit button.
     completeSubmit.addEventListener("click", function (){
         
         if (!completeInput.value){
             window.alert("Please enter your initials.");
         } else {
+// Object for the initials of the user and their score.
             var userScore = {
                 initials: completeInput.value.trim(),
                 score: timeScore
             }
-            
+// Gets values from the key "scores" in the local storage. 
             var storedScores = localStorage.getItem("scores");
             
+// If there are values the JSON parse method is used to make a JavaScript array
+// otherwise an array is made if there are no values stored.
             if (storedScores !== null) {
                 storedScores = JSON.parse(localStorage.getItem("scores"));
             } else {
                 storedScores = [];
             }
             
+// The current score is then pushed into the above array.
             storedScores.push(userScore);
+// The array is sorted in descending order.
             storedScores.sort((a, b) => b.score - a.score);
+// The array is then saved to local storage as a JSON string.
             localStorage.setItem("scores", JSON.stringify(storedScores));
+// The page changes to the high scores page where the score will be displayed.
             window.location.replace("./highscores.html");
         }
     });
